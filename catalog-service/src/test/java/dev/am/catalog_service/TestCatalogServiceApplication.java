30c2aad27aa3776a8af46aaeff3f69a591c101a2
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2025-10-07 23:38:18 -0400

    adding catalog service setup

---
+++ catalog-service/src/test/java/dev/am/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.am.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+	}
+
+}
